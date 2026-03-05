2dc0cbd667b81c6ee36e58af7ca9e97a0d3dd37e
WinPear <leminhthang24012006@gmail.com>
2026-03-05 07:52:04 +0700

    feat: Login View

---
+++ BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/LoginVIewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace BookStore_Management_AppDesktop.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+        private string _username = string.Empty;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+        private string _password = string.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<string> _carouselBooks = new();
+        public LoginViewModel()
+        {
+            LoadMockBooks();
+        }
+        private void LoadMockBooks()
+        {
+            CarouselBooks.Add("");
+            CarouselBooks.Add("");
+            CarouselBooks.Add("");
+            CarouselBooks.Add("");
+            CarouselBooks.Add("");
+            CarouselBooks.Add("");
+        }
+
+        private bool CanLogin()
+        {
+            return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanLogin))]
+        private void Login ()
+        {
+            System.Diagnostics.Debug.WriteLine($"Thực hiện đăng nhập cho: {Username}");
+        }
+        [RelayCommand]
+        private void NavigateToSignUp()
+        {
+            System.Diagnostics.Debug.WriteLine("Chuyển hướng sang Sign Up");
+        }
+    }
+}
---
+++ BookStoreSystem/BookStore_Management_AppDesktop/Views/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using BookStore_Management_AppDesktop.ViewModels;
+using Wpf.Ui.Controls;
+
+namespace BookStore_Management_AppDesktop.Views.Windows
+{
+    public partial class LoginWindow : FluentWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new LoginViewModel();
+        }
+    }
+}
