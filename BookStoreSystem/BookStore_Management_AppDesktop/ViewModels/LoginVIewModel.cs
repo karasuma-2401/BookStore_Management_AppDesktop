17aa7b13023bc5ae73eaa74ec244f74c6c6c195e
WinPear <leminhthang24012006@gmail.com>
2026-03-06 10:50:24 +0700

    feat: Update Sign Up

--- BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/LoginVIewModel.cs
+++ BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/LoginVIewModel.cs
@@ -11,11 +11,114 @@ namespace BookStore_Management_AppDesktop.ViewModels
     {
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private bool _isLoading = false;
+
+        [ObservableProperty]
+        private string _errorMessage = string.Empty;
+
+        [ObservableProperty]
+        private bool _isSuccessMessage = false;
+
+        #region Login
+
+        [ObservableProperty]
+        private bool _isLoginFormVisible = true;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
         private string _username = string.Empty;
 
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
         private string _password = string.Empty;
+
+        private bool CanLogin()
+        {
+            return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password) && !IsLoading;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanLogin))]
+        private async Task Login()
+        {
+            ErrorMessage = String.Empty;
+            IsLoading = true;
+
+            // await time to connect with BE
+            await Task.Delay(1000);
+
+            // admin username and password
+            if (Username != "admin" ||  Password != "123")
+            {
+                IsSuccessMessage = false;
+                ErrorMessage = "The username or password is incorrect!";
+                IsLoading = false;
+                return;
+            }
+
+            IsSuccessMessage = true;
+            ErrorMessage = "Login successful! Redirecting...";
+            Debug.WriteLine(ErrorMessage);
+
+
+
+            // call to feature
+            IsLoading = false;
+        }
+        #endregion
+        #region Sign up
+
+        [ObservableProperty]
+        private bool _isSignUpFormVisible = false;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _signUpFullName = string.Empty;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _signUpUsername = string.Empty;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _signUpPassword = string.Empty;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _signUpConfirmPassword = string.Empty;
+
+        private bool CanSignUp()
+        {
+            return !String.IsNullOrWhiteSpace(SignUpFullName) && 
+                    !String.IsNullOrWhiteSpace(SignUpUsername) &&
+                    !String.IsNullOrWhiteSpace(SignUpPassword) &&
+                    !String.IsNullOrWhiteSpace(SignUpConfirmPassword) &&
+                    SignUpPassword == SignUpConfirmPassword &&
+                    !IsLoading;
+        }
+        
+        [RelayCommand(CanExecute =nameof(CanSignUp))]
+        private async Task SignUp()
+        {
+            ErrorMessage = string.Empty;
+            IsLoading = true;
+
+            // await time 1s to connection with BE
+            await Task.Delay (1000);
+            if (SignUpUsername == "admin")
+            {
+                IsSuccessMessage = false;
+                ErrorMessage = "This username already exists.";
+                IsLoading = false;
+                return;
+            }
+            IsSuccessMessage = true;
+            ErrorMessage = "Registration successful! Redirecting to the login page...";
+            Debug.WriteLine(ErrorMessage);
+
+            // Return to Login Window
+            await Task.Delay(2000);
+            SwitchToLogin();
+
+            IsLoading = false;
+        }
+        #endregion
 
         [ObservableProperty]
         private ObservableCollection<string> _carouselBooks = new();
@@ -23,6 +126,7 @@ namespace BookStore_Management_AppDesktop.ViewModels
         {
             LoadMockBooks();
         }
+        // load background with full book from resource -> images
         private void LoadMockBooks()
         {
             string imagesFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Images");
@@ -40,20 +144,20 @@ namespace BookStore_Management_AppDesktop.ViewModels
             }
         }
 
-        private bool CanLogin()
+        [RelayCommand]
+        private void SwitchToSignUp()
         {
-            return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+            ErrorMessage = string.Empty;
+            IsLoginFormVisible = false;
+            IsSignUpFormVisible = true;
         }
 
-        [RelayCommand(CanExecute = nameof(CanLogin))]
-        private void Login ()
-        {
-            System.Diagnostics.Debug.WriteLine($"Thực hiện đăng nhập cho: {Username}");
-        }
         [RelayCommand]
-        private void NavigateToSignUp()
+        private void SwitchToLogin()
         {
-            System.Diagnostics.Debug.WriteLine("Chuyển hướng sang Sign Up");
+            ErrorMessage = string.Empty;
+            IsSignUpFormVisible = false;
+            IsLoginFormVisible = true;
         }
     }
 }
