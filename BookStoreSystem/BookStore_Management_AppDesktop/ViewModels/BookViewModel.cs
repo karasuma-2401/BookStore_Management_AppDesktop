ea16bb041b6914c9247593ff6852307af26ccc23
DonThuanUIT-24521736 <24521736@gm.uit.edu.vn>
2026-03-20 14:28:29 +0700

    feat: Successfully integrate real backend data (BookApiService) into the BookPage ListView.

--- BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/BookViewModel.cs
+++ BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/BookViewModel.cs
@@ -1,24 +1,28 @@
 ﻿using BookStore_Management_AppDesktop.Models;
+using BookStore_Management_AppDesktop.Services.API;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
-using static System.Reflection.Metadata.BlobBuilder;
+using System.Threading.Tasks;
 
 namespace BookStore_Management_AppDesktop.ViewModels
 {
     public partial class BookViewModel : ObservableObject
     {
+        private readonly BookApiService _apiService;
+
         [ObservableProperty]
-        private ObservableCollection<Book> _books;
+        private ObservableCollection<Book> _books = new ObservableCollection<Book>();
 
         public BookViewModel()
         {
-            Books = new ObservableCollection<Book>();
-            LoadFakeData();
+            _apiService = new BookApiService();
         }
 
-        private void LoadFakeData()
+        public async Task LoadDataAsync()
         {
-            
+            var booksFromApi = await _apiService.GetAllBooksAsync();
+
+            Books = new ObservableCollection<Book>(booksFromApi);
         }
     }
 }
