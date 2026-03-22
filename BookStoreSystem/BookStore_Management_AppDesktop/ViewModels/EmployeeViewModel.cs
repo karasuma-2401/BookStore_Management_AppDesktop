38223df4681561509b96933e6251ff6b0554ed15
Quý ông <le2092129@gmail.com>
2026-03-22 20:02:03 +0700

    feat: add avatar update functionality with MVVM and default gallery

--- BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/EmployeeViewModel.cs
+++ BookStoreSystem/BookStore_Management_AppDesktop/ViewModels/EmployeeViewModel.cs
@@ -1,42 +1,141 @@
 ﻿using BookStore_Management_AppDesktop.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Windows;
 
 namespace BookStore_Management_AppDesktop.ViewModels
 {
     public partial class EmployeeViewModel : ObservableObject
     {
+        // 1. QUẢN LÝ DANH SÁCH NHÂN VIÊN
         [ObservableProperty]
-        private ObservableCollection<Employee> _employees;
-        // Danh sách các lựa chọn cho số dòng trên mỗi trang
+        private ObservableCollection<Employee> _employees = new();
+
+        // 2. QUẢN LÝ AVATAR
+        // Ảnh thực tế đang hiển thị của người dùng
+        [ObservableProperty]
+        private string _currentAvatarPath = "pack://application:,,,/Resources/Images/default_user.png";
+
+        // Biến tạm để chứa ảnh đang chọn trong cửa sổ Modal (chưa lưu)
+        [ObservableProperty]
+        private string _tempAvatarPath;
+
+        // Danh sách ảnh mặc định (Đảm bảo Build Action của các file này là "Resource")
+        public ObservableCollection<string> DefaultAvatars { get; set; } = new()
+        {
+            "pack://siteoforigin:,,,/Resources/Images/Forged of Fire.webp",
+            "pack://siteoforigin:,,,/Resources/Images/In an Instant.webp",
+            "pack://siteoforigin:,,,/Resources/Images/In Five Year.webp",
+            "pack://siteoforigin:,,,/Resources/Images/Night Road.webp",
+            "pack://siteoforigin:,,,/Resources/Images/Promise Me.webp",
+            "pack://siteoforigin:,,,/Resources/Images/Magic Hour A novel.webp",
+            "pack://siteoforigin:,,,/Resources/Images/God of Malice A Dark College.webp",
+            "pack://siteoforigin:,,,/Resources/Images/Twenty Years Later.webp"
+        };
+
+        // 3. CẤU HÌNH PHÂN TRANG
         public List<int> PageSizeOptions { get; set; } = new List<int> { 4, 8, 12, 16 };
 
+        [ObservableProperty]
         private int _pageSize = 8;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => SetProperty(ref _pageSize, value); // Thông báo cho UI khi giá trị thay đổi
-        }
+
+        // CONSTRUCTOR
         public EmployeeViewModel()
         {
-            Employees = new ObservableCollection<Employee>();
-
             LoadFakeData();
         }
 
+        // --- COMMANDS ---
+
+        /// <summary>
+        /// Mở cửa sổ chỉnh sửa Avatar
+        /// </summary>
+        [RelayCommand]
+        private void OpenAvatarModal()
+        {
+            // Reset ảnh tạm về ảnh hiện tại trước khi mở modal
+            TempAvatarPath = CurrentAvatarPath;
+
+            var editWindow = new BookStore_Management_AppDesktop.Views.Windows.AvatarEditView(this);
+
+            if (Application.Current.MainWindow != null)
+            {
+                editWindow.Owner = Application.Current.MainWindow;
+            }
+
+            editWindow.ShowDialog();
+        }
+
+        /// <summary>
+        /// Chọn ảnh từ máy tính (Lưu vào biến tạm)
+        /// </summary>
+        [RelayCommand]
+        private void UploadFromComputer()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg;*.webp)|*.png;*.jpeg;*.jpg;*.webp",
+                Title = "Chọn ảnh đại diện"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TempAvatarPath = openFileDialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Chọn ảnh từ danh sách mặc định (Lưu vào biến tạm)
+        /// </summary>
+        [RelayCommand]
+        private void SelectDefaultAvatar(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                TempAvatarPath = path;
+            }
+        }
+
+        // Đóng cửa số avatar
+        [RelayCommand]
+        private void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+        /// <summary>
+        /// Xác nhận lưu thay đổi ảnh
+        /// </summary>
+        [RelayCommand]
+        private void SaveAvatarChange(Window window)
+        {
+            // Cập nhật ảnh chính thức từ ảnh tạm
+            CurrentAvatarPath = TempAvatarPath;
+
+            // Đóng cửa sổ
+            window?.Close();
+        }
+
+        // DỮ LIỆU GIẢ ĐỂ TEST
         private void LoadFakeData()
         {
-            for (int i = 0; i < 30; i++)
+            for (int i = 1; i <= 20; i++)
             {
-                _employees.Add(new Employee
+                Employees.Add(new Employee
                 {
-                    UserId = "AABBCC123",
-                    EmployeeId = "DDEEFF456",
-                    Username = "abc123",
-                    Password = "123456",
-                    Address = "123 Đường ABC",
-                    PhoneNumber = "0123456789",
-                    FullName = "Trần Văn A" + i
+                    UserId = i.ToString(),
+                    EmployeeId = "NV" + i.ToString("D3"),
+                    Username = "user_" + i,
+                    FullName = "Nhân viên " + i,
+                    Address = "Việt Nam",
+                    PhoneNumber = "090123456" + (i % 10)
                 });
             }
         }
