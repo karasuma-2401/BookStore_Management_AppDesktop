5d3fe5367302d2782b3a9b39c2db0ce333aaaa18
Huynh Gia Thinh <thinh871779@gmail.com>
2026-03-26 15:21:43 +0700

    feat: Author CRUD feat: Category CRUD feat: BookCategory CRUD docs: Update Enities

---
+++ BookStoreSystem/BookStoreManagement.API/Controllers/BookCategoriesController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using BookStoreManagement.API.Services.Interfaces;
+using BookStoreManagement.API.DTOs.BookCategories;
+using BookStoreManagement.API.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookStoreManagement.API.Controllers
+{
+    [Route("bookcategory")]
+    [ApiController]
+    //[Authorize]
+    public class BookCategoriesController : ControllerBase
+    {
+        private readonly IBookCategoryService _service;
+
+        public BookCategoriesController(IBookCategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BookCategoryCreateDto dto)
+        {
+            var bc = new BookCategory
+            {
+                BookId = dto.BookId,
+                CategoryId = dto.CategoryId
+            };
+
+            return Ok(await _service.Create(bc));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int bookId, int categoryId)
+        {
+            var deleted = await _service.Delete(bookId, categoryId);
+            if (!deleted) return NotFound();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BookStoreSystem/BookStoreManagement.API/Models/Category/CategoryCreateDto.cs
@@ -0,0 +1,7 @@
+namespace BookStoreManagement.API.DTOs.Categories
+{
+    public class CategoryCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
