08f3f16addcbd6067b562ae4a84ee96424b9ee44
Huynh Gia Thinh <thinh871779@gmail.com>
2026-03-30 21:27:57 +0700

    save local change

---
+++ BookStoreSystem/BookStoreManagement.API/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using BookStoreManagement.API.Services.Interfaces;
+using BookStoreManagement.API.DTOs.Categories;
+using BookStoreManagement.API.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookStoreManagement.API.Controllers
+{
+    [Route("category")]
+    [ApiController]
+    //[Authorize]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateDto dto)
+        {
+            var c = new Category { Name = dto.Name };
+            return Ok(await _service.Create(c));
+        }
+    }
+}
