6c29ad16a712be4adb5f62765fb768a4aeffa516
Huynh Gia Thinh <thinh871779@gmail.com>
2026-06-04 19:50:04 +0700

    feat: Authorize for admin to make debt and inventory reports

--- BookStoreSystem/BookStoreManagement.API/Controllers/DebtReportController.cs
+++ BookStoreSystem/BookStoreManagement.API/Controllers/DebtReportController.cs
@@ -1,11 +1,13 @@
 using BookStoreManagement.API.Models.DTOs;
 using BookStoreManagement.API.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreManagement.API.Controllers
 {
     [ApiController]
     [Route("debt-report")]
+    [Authorize(Roles = "admin")]
     public class DebtReportController : ControllerBase
     {
         private readonly IDebtReportService _service;
@@ -19,6 +21,27 @@ namespace BookStoreManagement.API.Controllers
         public async Task<IActionResult> Create(CreateDebtReportDTO dto)
         {
             var result = await _service.CreateReport(dto.Month, dto.Year, dto.CustomerId);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetReports([FromQuery] int month, [FromQuery] int year)
+        {
+            var result = await _service.GetReports(month, year);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _service.GetById(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpGet("customer/{customerId}")]
+        public async Task<IActionResult> GetByCustomer(int customerId)
+        {
+            var result = await _service.GetByCustomer(customerId);
             return Ok(result);
         }
     }
