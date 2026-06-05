fc10407097300972657d40bfd6e780241fe66188
WinPear <leminhthang24012006@gmail.com>
2026-06-05 12:05:57 +0700

    feat: delete filter

---
+++ BookStoreSystem/BookStore_Management_AppDesktop/Converters/NumberSignConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BookStore_Management_AppDesktop.Converters
+{
+    public class NumberSignConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal decimalValue && parameter is string param)
+            {
+                if (param == "Positive") return decimalValue > 0;
+                if (param == "Negative") return decimalValue < 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
