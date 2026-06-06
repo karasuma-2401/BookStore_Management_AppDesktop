0d289762b380d7d0a357e1362389e971d7cf9e71
Huynh Gia Thinh <thinh871779@gmail.com>
2026-06-06 22:06:57 +0700

    fix: Require phone number of Customer

--- BookStoreSystem/BookStoreManagement.API/Models/Customer/CustomerCreateDto.cs
+++ BookStoreSystem/BookStoreManagement.API/Models/Customer/CustomerCreateDto.cs
@@ -1,7 +1,7 @@
 public class CustomerCreateDto
 {
     public string Name { get; set; } = string.Empty;
-    public string? Phone { get; set; }
+    public required string Phone { get; set; }
     public string? Email { get; set; }
     public string? Address { get; set; }
 }
--- BookStoreSystem/BookStoreManagement.API/Models/Customer/CustomerResponseDto.cs
+++ BookStoreSystem/BookStoreManagement.API/Models/Customer/CustomerResponseDto.cs
@@ -2,7 +2,7 @@ public class CustomerResponseDto
 {
     public int CustomerId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Phone { get; set; }
+    public required string Phone { get; set; }
     public string? Email { get; set; }
     public string? Address { get; set; }
     public decimal Debt { get; set; }
