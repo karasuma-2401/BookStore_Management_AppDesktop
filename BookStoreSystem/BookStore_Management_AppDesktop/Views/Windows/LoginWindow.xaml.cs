5f676dd9cee188138f16b0d27dfca812b9986db4
WinPear <leminhthang24012006@gmail.com>
2026-06-08 20:17:05 +0700

    feat/login page

--- BookStoreSystem/BookStore_Management_AppDesktop/Views/Windows/LoginWindow.xaml.cs
+++ BookStoreSystem/BookStore_Management_AppDesktop/Views/Windows/LoginWindow.xaml.cs
@@ -1,47 +1,85 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using BookStore_Management_AppDesktop.ViewModels;
 using Wpf.Ui.Controls;
-using System.Windows.Input;
+
 namespace BookStore_Management_AppDesktop.Views.Windows
 {
     public partial class LoginWindow : FluentWindow
     {
-        private readonly Random _random = new Random();
+        private readonly Random _random = new();
+
         public LoginWindow(LoginViewModel viewModel)
         {
             InitializeComponent();
 
             DataContext = viewModel;
-            viewModel.CloseAction = () => this.Close();
+            viewModel.CloseAction = Close;
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState =
+                WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                WindowState =
+                    WindowState == WindowState.Maximized
+                        ? WindowState.Normal
+                        : WindowState.Maximized;
+            }
+            else
+            {
+                DragMove();
+            }
         }
 
         private void BookImagesBackground_Loaded(object sender, RoutedEventArgs e)
         {
-            // check isActived object is Image and have Transform
-            if (sender is System.Windows.Controls.Image img && img.RenderTransform is TranslateTransform transform)
+            if (sender is System.Windows.Controls.Image img &&
+                img.RenderTransform is TranslateTransform transform)
             {
-                // fly from 10px to 25px
                 double randomDistance = _random.Next(-25, -10);
-                // fly time from 2.5s to 4.5s
-                double randomDuration = _random.NextDouble() *2 + 2;
-                // delay from 0 to 2s
+                double randomDuration = _random.NextDouble() * 2 + 2;
                 double randomDelay = _random.NextDouble() * 2;
-                var fluctuateAnimation = new DoubleAnimation
+
+                var animation = new DoubleAnimation
                 {
                     To = randomDistance,
                     Duration = TimeSpan.FromSeconds(randomDuration),
                     BeginTime = TimeSpan.FromSeconds(randomDelay),
-                    // parameter for restart
-                    AutoReverse = true, 
-                    // infinite loop
+                    AutoReverse = true,
                     RepeatBehavior = RepeatBehavior.Forever,
-                    EasingFunction = new SineEase {EasingMode = EasingMode.EaseInOut}
+                    EasingFunction = new SineEase
+                    {
+                        EasingMode = EasingMode.EaseInOut
+                    }
                 };
-                transform.BeginAnimation(TranslateTransform.YProperty, fluctuateAnimation);
+
+                transform.BeginAnimation(
+                    TranslateTransform.YProperty,
+                    animation);
             }
         }
-
     }
 }
+
